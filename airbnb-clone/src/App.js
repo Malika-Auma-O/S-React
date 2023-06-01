5582e577a2e5e82ddbe185cc36df9da20e8ab850
Malika Auma Omwayi <mamajedeia@gmail.com>
2023-06-01 15:45:35 +0300

    updated components

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -6,16 +6,24 @@ import data from "./data";
 function App() {
   let info = data.map(item => {
     return ( 
-      <Card 
-        key={item.id}
-        img={item.coverImg} 
-        rating={item.stats.rating} 
-        reviewCount={item.stats.reviewCount}
-        location={item.location}
-        title={item.title}
-        price={item.price}
-        openSpots={item.openSpots}
+      <Card
+      key={item.id}
+      // give the entire item object instead of individual items
+      item={item}
+      // you can also use spread syntax instead of items above and remove the .item in Cards.js
+      // {...item}
       />
+      
+      // <Card 
+      //   key={item.id}
+      //   img={item.coverImg} 
+      //   rating={item.stats.rating} 
+      //   reviewCount={item.stats.reviewCount}
+      //   location={item.location}
+      //   title={item.title}
+      //   price={item.price}
+      //   openSpots={item.openSpots}
+      // />
     )
   })
 
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -1,27 +1,30 @@
-
+import star from "../images/star.png"
 
 
 export default function Card(props) {
+  console.log(props)
 
   let badgeText
-    if (props.openSpots === 0) {
+    if (props.item.openSpots === 0) {
         badgeText = "SOLD OUT"
-    } else if (props.location === "Online") {
+    } else if (props.item.location === "Online") {
         badgeText = "ONLINE"
     }
   return (
     <div className="card">
       {/* if open spots is truthy the SOLD OUT in the div element will be rendered, if falsy it will not render it*/}
       {/* {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>} */}
+      {/* add item to each element since we only have item in Apps.js */}
       {badgeText && <div className="card--badge">{badgeText}</div>}
-      <img className="card--image" src={props.img} alt="z#" />
+      <img className="card--image" src={props.item.coverImg} alt="z#" />
       <div className="card--stats">
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
-        <span className="gray">{props.location}</span>
+        <img className="card--star" src={star} alt="#" />
+        <span>{props.item.stats.rating}</span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
       </div>
-      <p>{props.title}</p>
-      <p> <span className="bold">From {props.price} </span>/ person</p>
+      <p>{props.item.title}</p>
+      <p> <span className="bold">From {props.item.price} </span>/ person</p>
       
 
     </div>
